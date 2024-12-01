f025ae42848bdcc0ab305db6835c67b27b6962eb
mmilanta <marco.milanta@gmail.com>
2024-12-01 14:46:07 +0100

    fix: support cyclic matches

--- src/proby/lib_dp.py
+++ src/proby/lib_dp.py
@@ -9,7 +9,8 @@ from time import time
 from enum import Enum
 import numpy as np
 from typing import NamedTuple
-
+from dataclasses import dataclass
+from itertools import chain
 
 
 class GameEnd(Enum):
@@ -131,7 +132,7 @@ def get_polynomial(graph: GameDirectedGraph, root: tuple) -> Polynome:
     return get_polynomial(calling_stack=[], root=root)
 
 
-def get_probs_from_frozen_probe_set(frozen_probe_set: FrozenProbeSet, values: dict[SymbolicVariable, np.ndarray]) -> Polynome:
+def get_probs_from_frozen_probe_set(frozen_probe_set: FrozenProbeSet, values: dict[SymbolicVariable, np.ndarray]) -> np.ndarray:
     shape = next(iter(values.values())).shape
     out = np.ones(shape)
 
@@ -180,27 +181,121 @@ def evaluate_graph(graph: GameDirectedGraph, root: tuple, **kwargs) -> np.ndarra
     return get_probs(calling_stack=[], root=root)
 
 
+
+def get_probs_from_frozen_probe_set_float(frozen_probe_set: FrozenProbeSet, values: dict[SymbolicVariable, float]) -> float:
+    out = 1.0
+
+    for sym_var, futures in frozen_probe_set:
+        for future in futures:
+            if future:
+                out = out * values[sym_var]
+            else:
+                out = out * (1 - values[sym_var])
+    return out
+
+
+@dataclass(frozen=True, eq=True)
+class Mon:
+    score: tuple | None
+    coeff: np.ndarray
+
+
+@dataclass(frozen=True, eq=True)
+class Pol:
+    mons: tuple[Mon]
+
+    def __add__(self, other: "Pol") -> "Pol":
+        monome_kdx: dict[tuple|None, np.ndarray] = {}
+        for monome in chain(self.mons, other.mons):
+            monome_kdx.setdefault(monome.score, 0)
+            monome_kdx[monome.score] += monome.coeff
+        return Pol(mons=tuple(Mon(score=k, coeff=v) for k, v in monome_kdx.items()))
+
+    def __radd__(self, other: "Pol") -> "Pol": 
+        if other == 0:
+            return self
+        else:
+            return other + self
+    def __mul__(self, other: float) -> "Pol":
+        return Pol(mons=tuple(Mon(score=mon.score, coeff=mon.coeff * other) for mon in self.mons))
+
+def evaluate_graph_float(graph: GameDirectedGraph, root: tuple, **kwargs) -> np.ndarray:
+    values: dict[SymbolicVariable, np.ndarray] = {}
+    shape = None
+    for k, val in kwargs.items():
+        assert isinstance(k, str)
+        assert isinstance(val, np.ndarray)
+        values[SymbolicVariable(id=k)] = val
+        if shape is None:
+            shape = val.shape
+        else:
+            assert shape == val.shape
+    probs: dict[tuple, Pol] = {}
+    def get_probs(calling_stack: list[tuple], root: tuple) -> Pol:
+        if root in calling_stack:
+            return Pol(mons=(Mon(score=root, coeff=1.0),))
+        if root not in graph:
+            raise ValueError(f"state {root} not in graph must be in graph")
+        if root in probs:
+            return probs[root]
+        calling_stack.append(root)
+        out_probs: Pol = Pol(mons=tuple([]))
+        for edge in graph[root]:
+            edge_probs = get_probs_from_frozen_probe_set(edge, values=values)
+            if graph[root][edge] == GameEnd.WIN:
+                out_probs += Pol(mons=(Mon(score=None, coeff=edge_probs),))
+            elif graph[root][edge] == GameEnd.LOSE:
+                continue
+            else:
+                next_probs = get_probs(calling_stack=calling_stack, root=graph[root][edge])
+                out_probs += next_probs * edge_probs
+        calling_stack.pop()
+
+        for mon in out_probs.mons:
+            if mon.score == root:
+                out_probs = Pol(mons=tuple(Mon(score=imon.score, coeff=imon.coeff / (1 - mon.coeff)) for imon in out_probs.mons if imon != mon))
+
+        if len(out_probs.mons) == 1 and out_probs.mons[0].score is None:
+            probs[root] = out_probs
+        return out_probs
+
+    prob_pol = get_probs(calling_stack=[], root=root)
+    assert len(prob_pol.mons) == 1 and prob_pol.mons[0].score is None
+
+    return prob_pol.mons[0].coeff
+
+
 if __name__ == "__main__":
     class TieBreakScore(NamedTuple):
         p1: int
         p2: int
         p1_serving: bool
-
+    N = 100
     def play_tie_break(score: TieBreakScore, p: Probe, q: Probe) -> TieBreakScore | GameEnd:
         sp = p if score.p1_serving else q
         if sp.run():
             score = TieBreakScore(p1=score.p1 + 1, p2=score.p2, p1_serving=score.p1_serving)
         else:
             score = TieBreakScore(p1=score.p1, p2=score.p2 + 1, p1_serving=score.p1_serving)
-        if score.p1 + score.p2 % 2 == 0:
+        if (score.p1 + score.p2) % 2 == 1:
             score = TieBreakScore(p1=score.p1, p2=score.p2, p1_serving=not score.p1_serving)
-        if score.p1 + score.p2 == 1:
-            score = TieBreakScore(p1=0, p2=0, p1_serving=score.p1_serving)
-        if max(score.p1, score.p2) == 2:
+        if score.p1 == score.p2 == N - 1:
+            score = TieBreakScore(p1=N - 2, p2=N - 2, p1_serving=score.p1_serving)
+        if max(score.p1, score.p2) == N:
             return GameEnd.WIN if score.p1 > score.p2 else GameEnd.LOSE
         return score
 
 
     dg = create_dg(score=TieBreakScore(p1=0, p2=0, p1_serving=True), play_fn=play_tie_break)
-    out = evaluate_graph(dg, p=np.arange(0,1,.01), q=np.arange(0,1,.01), root=TieBreakScore(p1=0, p2=0, p1_serving=True))
-    print(out)
+    def print_dg(dg: GameDirectedGraph):
+        out = ""
+        for state in dg:
+            out += str(state) + "\n"
+            for k in dg[state]:
+                out += "    * " + str(dg[state][k]) + "\n"
+        return out
+    # print(print_dg(dg))
+    s = time()
+    out = evaluate_graph_float(dg, p=np.arange(0,1, .01), q=np.arange(0,1, .01), root=TieBreakScore(p1=0, p2=0, p1_serving=True))      
+    print(time() - s)   
+    print(out)  
