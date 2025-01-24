fe22d3cba2e1649a50a11866642c199c91ed3cf5
Marco Milanta <marco.milanta@gmail.com>
2025-01-25 00:24:03 +0100

    compute_probability

--- main.c
+++ main.c
@@ -68,32 +68,48 @@ unsigned int* nav(unsigned int *graph, int id) {
     return graph + 2 + ((id - 2) * Pow2(k));
 }
 int n_edges(unsigned int *graph) {
+    // Number of outward edges, aways a power of 2
     return Pow2(graph[0]);
 }
 int n_nodes(unsigned int *graph) {
+    // Number of nodes considering the two special nodes (WIN and LOSE)
     return graph[1];
 }
-double _prob(unsigned int *graph, double* prob_cache, double * edge_probabilites, unsigned int id) {
-    if (prob_cache[id] >= 0) {
-        return prob_cache[id];
+void _prob(unsigned int graph[], char visiting[], double **prob_cache, double * edge_probabilites, unsigned int id) {
+    if (visiting[id] == 1){
+        prob_cache[id][0] = 0;
+        prob_cache[id][id] = 1;
     }
-    if (id == WIN) {
-        return 1;
-    } else if (id == LOSE) {
-        return 0;
-    } else {
-        unsigned int *node = nav(graph, id);
-        double prob = 0;
-        for (int i = 0; i < n_edges(graph); i++) {
-            prob += edge_probabilites[i] * _prob(graph, prob_cache, edge_probabilites, node[i]);
+    if (id == WIN)
+        return;
+    if (id == LOSE)
+        return;
+    if (prob_cache[id][0] >= -.5)
+        return;
+
+    visiting[id] = 1;
+    unsigned int *node = nav(graph, id);
+    double *p = malloc(n_nodes(graph) * sizeof(double));
+    for (int j = 0; j < n_nodes(graph); j++) {
+        p[j] = 0;
+    }
+    for (int i = 0; i < n_edges(graph); i++) {
+        _prob(graph, visiting, prob_cache, edge_probabilites, node[i]);
+        for (int j = 0; j < n_nodes(graph); j++) {
+            p[j] += edge_probabilites[i] * prob_cache[node[i]][j];
         }
-        prob_cache[id] = prob;
-        return prob;
     }
+    for (int j = 0; j < n_nodes(graph); j++) {
+        prob_cache[id][j] = p[j] / (1 - p[id]);
+    }
+
+    free(p);
+    visiting[id] = 0;
+    return;
 }
 
 
-double prob(unsigned int *graph, double * ps) {
+double prob(unsigned int *graph, double *ps) {
     double *edge_probabilites = malloc(n_edges(graph) * sizeof(double));
     for (int i = 0; i < n_edges(graph); i++) {
         edge_probabilites[i] = 1;
@@ -105,11 +121,26 @@ double prob(unsigned int *graph, double * ps) {
         }
     }
 
-    double *prob_cache = malloc(n_nodes(graph) * sizeof(double));
-    for (int i = 0; i < n_nodes(graph); i++) {
-        prob_cache[i] = -1.0;
+    double **prob_cache = malloc(n_nodes(graph)  * sizeof(double *));
+    char *visiting = malloc(n_nodes(graph) * sizeof(char));
+    prob_cache[WIN] = malloc(n_nodes(graph)  * sizeof(double));
+    prob_cache[LOSE] = malloc(n_nodes(graph)  * sizeof(double));
+    for (int j = 0; j < n_nodes(graph); j++) {
+        visiting[j] = 0;
+        prob_cache[WIN][j] = 0;
+        prob_cache[LOSE][j] = 0;
     }
-    double out = _prob(graph, prob_cache, edge_probabilites, 2);
+    prob_cache[WIN][0] = 1;
+
+    for (int i = 2; i < n_nodes(graph); i++) {
+        prob_cache[i] = malloc(n_nodes(graph)  * sizeof(double));
+        prob_cache[i][0] = -1; // Cache is empty
+        for (int j = 1; j < n_nodes(graph); j++) {
+            prob_cache[i][j] = 0;
+        }
+    }
+    _prob(graph, visiting, prob_cache, edge_probabilites, 2);
+    double out = prob_cache[2][0];
     free(edge_probabilites);
     free(prob_cache);
     return out;
@@ -118,10 +149,12 @@ double prob(unsigned int *graph, double * ps) {
 int main() {
 
     unsigned int *graph = fetch_data();
+    double p;
     double x;
-    for (double i = 0; i < 10; i++) {
-        x = prob(graph, (double[]){0.5, 0.5, i/10});
-        printf("%f -> %f\n", i/100, x);
+
+    for (double i = 5; i < 10; i++) {
+        p = 0.5 + i/1000;
+        x = prob(graph, (double[]){p});
+        printf("%f -> %f\n", p, x);
     }
-    printf("Probability: %f\n", x);
 }
